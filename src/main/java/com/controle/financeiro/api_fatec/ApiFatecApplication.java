16a39a16d53ce6eff2b75c3d0088206b06c29df6
Bruno Henrique <brunocamargo1127@hotmail.com>
2024-08-21 19:50:41 -0300

    Exemplos de Controllers

--- src/main/java/com/controle/financeiro/api_fatec/ApiFatecApplication.java
+++ src/main/java/com/controle/financeiro/api_fatec/ApiFatecApplication.java
@@ -9,5 +9,4 @@ public class ApiFatecApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/controle/financeiro/api_fatec/controllers/Exemplo1Controller.java
@@ -0,0 +1,36 @@
+package com.controle.financeiro.api_fatec.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exercicio")
+public class Exemplo1Controller {
+
+    @GetMapping()
+    public String HelloWorld(){
+        return "Hello";
+    }
+
+    @GetMapping("/hello1")
+    public String HelloWorld1(){
+        return "Hello1";
+    }
+
+    //PathVarible = anotacão para definir que a variavel nome que é o tipo String será recebida pelo parametro (nome)
+    @GetMapping("/reverter-nome/{nome}")
+    String reverterNome (@PathVariable String nome){
+        return new StringBuilder(nome).reverse().toString();
+    }
+
+    @GetMapping("/ar-ou-impar/{numero}")
+    String verificarParImpar (@PathVariable Integer numero){
+        if(numero % 2 == 0){
+            return "Par";
+        } else {
+            return "Ímpar";
+        }
+    }
+}
